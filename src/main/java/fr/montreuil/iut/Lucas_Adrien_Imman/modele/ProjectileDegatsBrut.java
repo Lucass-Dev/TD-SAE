b48e87563da9cb1e6cbdb2e6da41ca97fbc23ad2
Immannn <i.amimman0.0@gmail.com>
2023-06-09 15:35:50 +0200

    Projectile ProjectileRalentisseur

--- src/main/java/fr/montreuil/iut/Lucas_Adrien_Imman/modele/ProjectileDegatsBrut.java
+++ src/main/java/fr/montreuil/iut/Lucas_Adrien_Imman/modele/ProjectileDegatsBrut.java
@@ -14,7 +14,30 @@ public class ProjectileDegatsBrut extends  Projectile{
         e.reductionPv(10);
     }
 
+    @Override
+    public boolean cibleAtteint() {
+        int range = 16 ;
+        return ((this.getY()-range<=e.getY() && e.getY()<= this.getY()+range) && (this.getX()-range<=e.getX() && e.getX() <= this.getX()+range)) ;
+    }
 
+    @Override
+    public void moveProjectile() {
+        double  posX = e.getX() - this.getX() ;
+        double  posY = e.getY() - this.getY() ;
+        double dirX, dirY;
+
+        double totalDis = Math.sqrt(posX * posX + posY * posY);
+
+        dirX = posX  / totalDis  ;
+        dirY = posY  / totalDis ;
+
+
+        double newPosX = this.getX() + (5 * dirX);
+        double newPosY = this.getY() + (5 * dirY);
+
+        setX((int) newPosX);
+        setY((int) newPosY);
+    }
 
 
 }
