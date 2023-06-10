3923d03d7a5b5aec5fcb0f69d69050fe379aca61
Lucass-Dev <liger.lucas33@gmail.com>
2023-06-10 12:41:25 +0200

    put damage on DotSH

--- src/main/java/fr/montreuil/iut/Lucas_Adrien_Imman/modele/DotSH.java
+++ src/main/java/fr/montreuil/iut/Lucas_Adrien_Imman/modele/DotSH.java
@@ -4,7 +4,7 @@ import javafx.scene.layout.Pane;
 
 public class DotSH extends Ennemy{
     public DotSH(int x, int y, Pane levelPane, Level level, Player player) {
-        super(x, y, levelPane, level, 0, 30, player, 7, 30);
+        super(x, y, levelPane, level, 0, 30, player, 7, 30, 5);
     }
 
 
