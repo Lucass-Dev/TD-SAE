47297c1eec2bf1b0aade41f36f0330be26dba0a4
alamchichi <ayoub.lamchichi.info@gmail.com>
2023-10-11 14:42:49 +0200

    ajout structure algoDeplacements

--- src/main/java/fr/montreuil/iut/Lucas_Adrien_Imman/Deplaçable.java
+++ src/main/java/fr/montreuil/iut/Lucas_Adrien_Imman/Deplaçable.java
@@ -1,13 +1,26 @@
 package fr.montreuil.iut.Lucas_Adrien_Imman;
 
+import fr.montreuil.iut.Lucas_Adrien_Imman.modele.Deplacement.ModeDeplacement;
+
 public abstract class Deplaçable extends Acteur {
-    public Deplaçable(int x, int y,String id) {
-        super(x,y,id);
+
+    private ModeDeplacement modeDeplacement;
+
+
+    public Deplaçable(int x, int y, String id, ModeDeplacement modeDeplacement) {
+        super(x, y, id);
+        this.modeDeplacement = modeDeplacement;
     }
+
 
     public abstract boolean isOnBound();
 
-    public abstract void algoDeplacement();
+
+
+    public void algoDeplacement() {
+        modeDeplacement.seDeplacer(this);
+    }
+    ;
 
     public abstract boolean isOnObjective();
 }
