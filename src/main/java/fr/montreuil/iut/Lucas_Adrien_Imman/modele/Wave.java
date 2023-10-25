70e85a31ba6043bcf5dc236618fb5040f82072dc
adri1ps <adrangab3>
2023-10-25 16:51:21 +0200

    Amelioration de la redondance de code

--- src/main/java/fr/montreuil/iut/Lucas_Adrien_Imman/modele/Wave.java
+++ src/main/java/fr/montreuil/iut/Lucas_Adrien_Imman/modele/Wave.java
@@ -37,40 +37,62 @@ public class Wave {
         for (int i = 0; i < size; i++) {
             switch ((int) ((Math.random() * (6 - 1)) + 1)){
                 case 1 -> {
-                    this.actualWave.add(new DotSH(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction));
+                    this.actualWave.add(createDotSH(ground, levelPane, player, environment, direction));
                 }
                 case 2 -> {
                     if (this.actualWaveNumber.get() <= 5){
-                        this.actualWave.add(new DotSH(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction));
+                        this.actualWave.add(createDotSH(ground, levelPane, player, environment, direction));
                     }else{
-                        this.actualWave.add(new Archive(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction));
+                        this.actualWave.add(createArchive(ground, levelPane, player, environment, direction));
                     }
                 }
                 case 3 -> {
                     if (this.actualWaveNumber.get() <= 10){
-                        this.actualWave.add(new DotSH(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction));
+                        this.actualWave.add(createDotSH(ground, levelPane, player, environment, direction));
                     }else{
-                        this.actualWave.add(new Virus(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction));
+                        this.actualWave.add(createVirus(ground, levelPane, player, environment, direction));
                     }
                 }
                 case 4 -> {
                     if (this.actualWaveNumber.get() <= 15){
-                        this.actualWave.add(new DotSH(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction));
+                        this.actualWave.add(createDotSH(ground, levelPane, player, environment, direction));
                     }else{
-                        this.actualWave.add(new Scam(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction));
+                        this.actualWave.add(createScam(ground, levelPane, player, environment, direction));
                     }
                 }
                 case 5 -> {
                     if (this.actualWaveNumber.get() <= 20){
-                        this.actualWave.add(new DotSH(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction));
+                        this.actualWave.add(createDotSH(ground, levelPane, player, environment, direction));
                     }else{
-                        this.actualWave.add(new DotExe(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction));
+                        this.actualWave.add(createDotExe(ground, levelPane, player, environment, direction));
                     }
                 }
             }
         }
         setActualWaveNumber(actualWaveNumber.get() + 1);
     }
+
+    public Ennemy createDotSH(Ground ground, Pane levelPane, Player player, Environment environment, int direction){
+        return new DotSH(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction);
+    }
+
+    public Ennemy createArchive(Ground ground, Pane levelPane, Player player, Environment environment, int direction){
+        return new Archive(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction);
+    }
+
+    public Ennemy createVirus(Ground ground, Pane levelPane, Player player, Environment environment, int direction){
+        return new Virus(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction);
+    }
+
+    public Ennemy createScam(Ground ground, Pane levelPane, Player player, Environment environment, int direction){
+        return new Scam(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction);
+    }
+
+    public Ennemy createDotExe(Ground ground, Pane levelPane, Player player, Environment environment, int direction){
+        return new DotExe(ground.getStartTilePos()[0]*32 +16, ground.getStartTilePos()[1]*32 +16, levelPane, environment, player, direction);
+    }
+
+
 
     public ArrayList<Ennemy> getActualWave() {
         return actualWave;
